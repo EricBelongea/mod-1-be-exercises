d91db195c8fdec9c5327ea527873ded588038095
Eric Belongea <eric.belongea@gmail.com>
2023-08-30 12:21:05 -0600

    Potluck: adds up to hashes

--- practice_independent_challenges/week2/potluck/lib/potluck.rb
+++ practice_independent_challenges/week2/potluck/lib/potluck.rb
@@ -1,3 +1,4 @@
+require'pry'
 class Potluck
   attr_reader :date, :dishes
 
@@ -11,8 +12,24 @@ class Potluck
   end
 
   def get_all_from_category(dish_category)
-    categorized_dishes = dishes.select do |dish|
-      dish.category == dish_category
-    end
+    categorized_dishes = dishes.select { |dish| dish.category == dish_category }
+  end
+
+  def menu
+    # big_menu = {:appetizer => apps.sort, :entre => entres.sort, :dessert => desserts.sort}
+
+    apps = dishes.select { |dish| dish.category == :appetizer }
+    entres = dishes.select { |dish| dish.category == :entre }
+    desserts = dishes.select { |dish| dish.category == :dessert }
+
+    big_menu = {"apps", "entres", "dessert"}
+
+    big_menu["apps"] = apps.sort
+    big_menu["entres"] = entres.sort
+    big_menu["desserts"] = desserts.sort
+
+    binding.pry
+
+    big_menu
   end
 end
