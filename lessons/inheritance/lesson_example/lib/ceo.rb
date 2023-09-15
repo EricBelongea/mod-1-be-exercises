db2dcb6577d11224d1f08831ecb2535d644b699f
Eric Belongea <eric.belongea@gmail.com>
2023-09-15 09:39:52 -0600

    Inheritance, finish lesson_example

--- lessons/inheritance/lesson_example/lib/ceo.rb
+++ lessons/inheritance/lesson_example/lib/ceo.rb
@@ -1,11 +1,13 @@
-class Ceo
+require 'employee'
+class Ceo < Employee
 
   attr_reader :base_salary,
               :bonus
 
-  def initialize(base_salary, bonus)
+  def initialize(name, id, base_salary, bonus)
     @base_salary = base_salary
     @bonus       = bonus
+    super(name, id)
   end
 
 end
--- lessons/inheritance/lesson_example/spec/ceo_spec.rb
+++ lessons/inheritance/lesson_example/spec/ceo_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe Ceo do
 
   describe "setup" do
     it "exists" do
-      ceo = Ceo.new(15,20)
+      ceo = Ceo.new("Jeff", 12345, 15,20)
     end
     it "has base_salary and bonus attributes" do
-      ceo = Ceo.new(15,20)
+      ceo = Ceo.new("Jeff", 12345, 15,20)
       expect(ceo.base_salary).to eq(15)
       expect(ceo.bonus).to eq(20)
     end
@@ -15,7 +15,18 @@ RSpec.describe Ceo do
 
   describe "Has a name, ID, and total_compensation" do
     # Write tests to prove that a Ceo instance can have a Name and ID number, like any other Employee.
-    # Also, the CEO should be able to receive a total compensation. Write at least one test that proves they can access this method.
+    it 'has atts' do
+      ceo = Ceo.new("Jeff", 12345, 15,20)
+      expect(ceo.name).to eq("Jeff")
+      expect(ceo.id).to eq(12345)
+    end
+
+    # Also, the CEO should be able to receive a total compensation. Write at least one test that 
+    # proves they can access this method.
+    it 'can have total comp' do
+      ceo = Ceo.new("Jeff", 12345, 15,20)
+      expect(ceo.total_compensation).to eq(35)
+    end
   end
 
 end
