3b296f977b5928e385c2515cdc9ac5042951ee0e
Eric Belongea <eric.belongea@gmail.com>
2023-09-15 09:49:23 -0600

    Inheritance ex1, shapes

--- lessons/inheritance/exercise_1/lib/rectangle.rb
+++ lessons/inheritance/exercise_1/lib/rectangle.rb
@@ -14,14 +14,4 @@ class Rectangle
   def perimeter
     (2 * (@length + @width))
   end
-
-  # this class should have the following properties:
-  # - color
-  # - length
-  # - width
-
-  #It should have the following methods:
-  # - area (width * length)
-  # - perimeter (2 * (length + width))
-
 end
--- lessons/inheritance/exercise_1/lib/shape.rb
+++ lessons/inheritance/exercise_1/lib/shape.rb
@@ -1,11 +1,17 @@
 class Shape
+  attr_reader :color, :width, :length
 
-  # This class should be a parent that creates the following properties:
-  # - color
-  # - width
-  # - length
-  # It should also have the following behaviors:
-  # - area (width * length)
-  # - perimeter (2 * (length + width))
+  def initialize(color, length, width)
+    @color = color
+    @length = length
+    @width = width
+  end
 
+  def area
+    @width * @length
+  end
+
+  def perimeter
+    (2 * (width + length))
+  end
 end
