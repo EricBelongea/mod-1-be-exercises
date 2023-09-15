cb231c82b6d82cc5355010713c5b1fede63fd128
Eric Belongea <eric.belongea@gmail.com>
2023-09-15 12:23:36 -0600

    Normalizes zipcodes

--- lessons/csv_files/event_manager/lib/event_manager.rb
+++ lessons/csv_files/event_manager/lib/event_manager.rb
@@ -1,9 +1,9 @@
 # ./lib/event_manager.rb
 require "csv"
-puts "EventManager initialized."
+# puts "EventManager initialized."
 
 ### Reads the CSV
-contents = File.read "./data/event_attendees.csv"
+# contents = File.read "./data/event_attendees.csv"
 # puts contents
 
 ### Reads Line by line
@@ -52,10 +52,59 @@ contents = File.read "./data/event_attendees.csv"
 
 
 ### The above is so common that they created the '.each_with_index' method for this purpose. Similar to '.flat_map'
-lines = File.readlines "./data/event_attendees.csv"
-lines.each_with_index do |line,index|
-  next if index == 0
-  columns = line.split(",")
-  name = columns[2]
-  puts name
+# lines = File.readlines "./data/event_attendees.csv"
+# lines.each_with_index do |line,index|
+#   next if index == 0
+#   columns = line.split(",")
+#   name = columns[2]
+#   puts name
+# end
+
+
+##### Start Iteration One #####
+
+
+### We are using Ruby's built in method, CSV, which allows us to inform the method that there is a header. 
+# contents = CSV.open "./data/event_attendees.csv", headers: true
+# contents.each do |row|
+#   name = row[2]
+#   puts name
+# end
+
+
+### Using Ruby's built in CSV method again, but now we are adding in another parameter to convert the header indexes to sym.
+# contents = CSV.open "./data/event_attendees.csv", headers: true, header_converters: :symbol
+# contents.each do |row|
+#   name = row[:last_name]
+#   puts name
+# end
+
+
+### This will print all of the names and zipcodes. If we uncomment and run the code we will be able to see that not all zipcode 
+### are uniform so we will need to do some zipcode manipulation. 
+# contents = CSV.open "./data/event_attendees.csv", headers: true, header_converters: :symbol
+# contents.each do |row|
+#   name = row[:first_name]
+#   zipcode = row[:zipcode]
+#   puts "#{name} #{zipcode}"
+# end
+
+
+
+### Below we format the zipcodes to all be standardized. 
+contents = CSV.open './data/event_attendees.csv', headers: true, header_converters: :symbol
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+
+  if zipcode.nil?
+    zipcode = "00000"
+  elsif zipcode.length < 5
+    zipcode = zipcode.rjust 5, "0"
+  elsif zipcode.length > 5
+    zipcode = zipcode[0..4]
+  end
+
+  puts "#{name} #{zipcode}"
 end
