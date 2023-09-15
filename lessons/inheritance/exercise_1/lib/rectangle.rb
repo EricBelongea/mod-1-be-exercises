5154a1627453ac929faebba514ba4729f8de3265
Eric Belongea <eric.belongea@gmail.com>
2023-09-15 09:46:58 -0600

    Inheritance ex1, rectangle class

--- lessons/inheritance/exercise_1/lib/rectangle.rb
+++ lessons/inheritance/exercise_1/lib/rectangle.rb
@@ -1,4 +1,19 @@
 class Rectangle
+  attr_reader :color, :width, :length
+
+  def initialize(color, length, width)
+    @color = color
+    @length = length
+    @width = width
+  end
+
+  def area
+    @width * @length
+  end
+
+  def perimeter
+    (2 * (@length + @width))
+  end
 
   # this class should have the following properties:
   # - color
