6b106ec5e55dfe96b61c4d0d16a08726f18f255b
Eric Belongea <eric.belongea@gmail.com>
2023-09-20 10:07:26 -0600

    Update: uses more mocks and stubs

--- lessons/mocks_stubs/bob_ross/spec/bob_spec.rb
+++ lessons/mocks_stubs/bob_ross/spec/bob_spec.rb
@@ -31,6 +31,7 @@ RSpec.describe Bob do
       expect(bob.paints).to eq([paint_1, paint_2])
     end
 
+    # Below shows Mocks & Stubs in unison 
     it 'can return paint colors' do
       bob = Bob.new
       paint_1 = double("Alizarin Crimson")
@@ -45,10 +46,19 @@ RSpec.describe Bob do
       expect(bob.paint_colors).to eq(["Alizarin Crimson", "Van Dyke Brown"])
     end
 
-    xit 'can calculate total paint amount' do
+    # You don't need a Mock to use a Stub: We used both mock and stub.
+    it 'can calculate total paint amount' do
       bob = Bob.new
-      paint_1 = Paint.new("Alizarin Crimson", 42)
-      paint_2 = Paint.new("Van Dyke Brown", 25)
+      paint_1 = double
+      paint_2 = double
+
+      # This is using Stubs. We only need to access the amount, so we don't include the name.
+      allow(paint_1).to receive(:amount).and_return(42)
+      allow(paint_1).to receive(:color).and_return("Alizarin Crimson")
+      # The above allows you to create multiple types of attributes to one object instance.
+      # require 'pry'; binding.pry
+      allow(paint_2).to receive(:amount).and_return(25)
+
 
       bob.add_paint(paint_1)
       bob.add_paint(paint_2)
